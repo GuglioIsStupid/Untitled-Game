6e343711596287406c293063af2b3dcdf074764a
GuglioIsStupid <bradsbluetab@gmail.com>
2022-08-16 18:47:18 -0400

    fix jump for lvl 2

--- src/love/data/player.lua
+++ src/love/data/player.lua
@@ -49,39 +49,39 @@ function player.update(dt)
                 player.x = (player.x + (player.speed/#blocks) * dt) / #blocks
             end
         end
-        if input:pressed("jump") then
-            if (curLevel ~= 2 and not jumping) or (curLevel == 2) then
-                jumping = true
-                if jumpsound[#jumpsound]:isPlaying() then
-                    jumpsound[#jumpsound] = jumpsound[#jumpsound]:clone()
-                    jumpsound[#jumpsound]:play()
-                else
-                    jumpsound[#jumpsound]:play()
-                end
-                for i = 2, #jumpsound do
-                    if not jumpsound[i]:isPlaying() then
-                        jumpsound[i] = nil -- Nil afterwords to prevent memory leak
-                    end --                             maybe, idk how love2d works lmfao
-                end
-                Timer.tween(
-                    0.7,
-                    player,
-                    {y = player.y - 100},
-                    "out-quad",
-                    function()
-                        Timer.tween(
-                            0.55,
-                            player,
-                            {y = player.y + 100},
-                            "in-quad",
-                            function()
-                                landsound:play()
-                                jumping = false
-                            end
-                        )
-                    end
-                )
+    end
+    if input:pressed("jump") then
+        if (curLevel ~= 2 and not jumping) or (curLevel == 2) then
+            jumping = true
+            if jumpsound[#jumpsound]:isPlaying() then
+                jumpsound[#jumpsound] = jumpsound[#jumpsound]:clone()
+                jumpsound[#jumpsound]:play()
+            else
+                jumpsound[#jumpsound]:play()
             end
+            for i = 2, #jumpsound do
+                if not jumpsound[i]:isPlaying() then
+                    jumpsound[i] = nil -- Nil afterwords to prevent memory leak
+                end --                             maybe, idk how love2d works lmfao
+            end
+            Timer.tween(
+                0.7,
+                player,
+                {y = player.y - 100},
+                "out-quad",
+                function()
+                    Timer.tween(
+                        0.55,
+                        player,
+                        {y = player.y + 100},
+                        "in-quad",
+                        function()
+                            landsound:play()
+                            jumping = false
+                        end
+                    )
+                end
+            )
         end
     end
 end
